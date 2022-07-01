7683dce72b92217afaf3455288f46ac5da6e8b03
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-07-01 15:28:05 -0300

    Reaload random image

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
@@ -9,10 +11,25 @@ let int = setInterval(cont, 30)
 
 function cont(){
   load++
+
   if(load > 99) {
     clearInterval(int)
   }
+
   loadText.innerHTML = `${load}%`
   loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load, 0, 100, 3, 0)}rem)` 
+  bg.style.filter = `blur(${scale(load, 0, 100, 3, 0)}rem)`
+
+  if(load == 100){
+    sleep(2500);
+    location.reload()
+  }  
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
 }
